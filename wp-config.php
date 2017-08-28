5a7ee0bdf7d117883b3790fc37081b239035f715
Thang Nguyen <thang.nq@sutrixsolutions.com>
2017-08-28 09:19:53 +0700

    Import Diner theme

--- wp-config.php
+++ wp-config.php
@@ -32,10 +32,10 @@ define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 'utf8_general_ci');
 
 /**#@+
  * Authentication Unique Keys and Salts.
